fd85cb8cb774ee79a41a2f8d8318d487a8dce370
LAPTOP-2P2KMQF4\HP <nasitsmeet28@gmail.com>
2025-11-01 00:46:53 +0100

    day 6

--- Iot_Management_System/Models/SensorModal.cs
+++ Iot_Management_System/Models/SensorModal.cs
@@ -19,7 +19,7 @@
 
     public class Sensor_Units
     {
-        public int Id { get; set; }
+        public int SensorID { get; set; }
         public string? SensorName { get; set; }
         public string? MeasuredParameter { get; set; }
         public string? Unit { get; set; }
@@ -27,42 +27,6 @@
     }
 
 
-
-
-    public class GetEmployee
-    {
-        public long Id { get; set; }
-
-        public string? EmpCode { get; set; }
-
-
-        public string? EmpName { get; set; }
-
-        public string? Address1 { get; set; }
-
-        public string? Address2 { get; set; }
-
-        public string? Country { get; set; }
-        public long? CountryId { get; set; }
-
-        public string? State { get; set; }
-        public long? StateId { get; set; }
-
-        public string? City { get; set; }
-        public long? CityId { get; set; }
-
-        public long? MobileNo { get; set; }
-
-        public long? PhoneNo { get; set; }
-
-        public string? Email { get; set; }
-        public long? IsDeleted { get; set; }
-
-        public string? EmpDesignation { get; set; }
-        public long? EmpSalary { get; set; }
-
-
-    }
 
 
  
