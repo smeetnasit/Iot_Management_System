d835f360454a2b8747c828000b55ffc2b62cc512
LAPTOP-2P2KMQF4\HP <nasitsmeet28@gmail.com>
2026-03-13 16:42:18 +0100

    add alert management with acknowledge and resolve

---
+++ Iot_Management_System_API/AlertStatusRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Iot_Management_System_API
+{
+    public class AlertStatusRequest
+    {
+        public string Status { get; set; } = string.Empty;
+    }
+}
--- Iot_Management_System_API/Repository/Sensor_Repository.cs
+++ Iot_Management_System_API/Repository/Sensor_Repository.cs
@@ -125,166 +125,33 @@ namespace Iot_Management_System_API.Repository
             }
         }
 
+        public async Task<List<SensorReading>> GetAlertReadings()
+        {
+            using (var connection = _dBContext.CreateConnection())
+            {
+                var readings = await connection.QueryAsync<SensorReading>("Sp_Get_Alert_Readings",commandType: CommandType.StoredProcedure);
+                return readings.ToList();
+            }
+        }
+
+        public async Task<bool> UpdateAlertStatus(int readingId, string status)
+        {
+            using (var connection = _dBContext.CreateConnection())
+            {
+                var param = new DynamicParameters();
+                param.Add("@Reading_Id", readingId,DbType.Int32, ParameterDirection.Input);
+                param.Add("@Alert_Status", status,DbType.String, ParameterDirection.Input);
+
+                var affectedRows = await connection.ExecuteScalarAsync<int>("Sp_Update_Alert_Status",param,commandType: CommandType.StoredProcedure);
+
+                return affectedRows > 0;
+            }
+        }
+
+
     }
 
-    //public async Task<CommonResponse> MasterCountryInsert(int id)
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-    //        try
-    //        {
-    //            CommonResponse commonResponse = new CommonResponse();
-
-    //            DynamicParameters param = new DynamicParameters();
-
-    //             param.Add("@MasterCountryId", id, dbType: DbType.String, direction: ParameterDirection.Input);
-
-
-
-    //            var task = connection.QueryMultiple("Proc_CountryMaster_Add", param, commandTimeout: 600, commandType: CommandType.StoredProcedure);
-    //            return task.Read<CommonResponse>().FirstOrDefault();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            throw ex;
-    //        }
-    //    }
-    //}
-
-
-
-
-    //public async Task<List<Country>> GetCountries()
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-
-    //        try
-    //        {
-    //            var items = await connection.QueryAsync<Country>("Proc_Smeet_GetCountries", commandType: CommandType.StoredProcedure);
-    //            return items.ToList();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"An error occurred: {ex.Message}");
-    //            throw;
-    //        }
-    //    }
-    //}
-
-
-    //public async Task<List<CountryMaster>> GetMasterCountries()
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-
-    //        try
-    //        {
-    //            var items = await connection.QueryAsync<CountryMaster>("Proc_Smeet_GetMasterCountries", commandType: CommandType.StoredProcedure);
-    //            return items.ToList();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"An error occurred: {ex.Message}");
-    //            throw;
-    //        }
-    //    }
-    //}
-
-    //public async Task<List<State>> GetStates(int CountryId)
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-
-    //        try
-    //        {
-    //            DynamicParameters param = new DynamicParameters();
-    //            param.Add("@CountryId", CountryId, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            var items = await connection.QueryAsync<State>("Proc_Smeet_GetStates",param, commandType: CommandType.StoredProcedure);
-    //            return items.ToList();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"An error occurred: {ex.Message}");
-    //            throw;
-    //        }
-    //    }
-    //}
-
-
-    //public async Task<List<City>> GetCities(int StateId)
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-
-    //        try
-    //        {
-    //            DynamicParameters param = new DynamicParameters();
-    //            param.Add("@StateId", StateId, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            var items = await connection.QueryAsync<City>("Proc_Smeet_GetCities", param, commandType: CommandType.StoredProcedure);
-    //            return items.ToList();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"An error occurred: {ex.Message}");
-    //            throw;
-    //        }
-    //    }
-    //}
-
-
-    //public async Task<List<GetEmployee>> GetEmployees()
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-    //        try
-    //        {
-    //            var items = await connection.QueryAsync<GetEmployee>("Proc_Smeet_GetAllEmployees", commandType: CommandType.StoredProcedure);
-    //            return items.ToList();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"An error occurred: {ex.Message}");
-    //            throw;
-    //        }
-    //    }
-
-    //}
-
-    //public async Task<CommonResponse> Emp_Update(Sensor emp)
-    //{
-    //    using (var connection = _dBContext.CreateConnection())
-    //    {
-
-    //        try
-    //        {
-    //            DynamicParameters param = new DynamicParameters();
-    //            param.Add("@Id", emp.Id, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@EmpCode", emp.EmpCode, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@EmpName", emp.EmpName, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@Address1", emp.Address1, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@Address2", emp.Address2, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@Country", emp.Country, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@State", emp.State, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@City", emp.City, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@MobileNo", emp.MobileNo, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@PhoneNo", emp.PhoneNo, dbType: DbType.Int64, direction: ParameterDirection.Input);
-    //            param.Add("@Email", emp.Email, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@EmpDesignation", emp.EmpDesignation, dbType: DbType.String, direction: ParameterDirection.Input);
-    //            param.Add("@EmpSalary", emp.EmpSalary, dbType: DbType.Int64, direction: ParameterDirection.Input);
-
-    //            var task = connection.QueryMultiple("Proc_Tbl_Emp_Update", param, commandTimeout: 600, commandType: CommandType.StoredProcedure);
-    //            return task.Read<CommonResponse>().FirstOrDefault();
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            throw ex;
-    //        }
-    //    }
-    //}
-
-
-
+    
 
 
     //public async Task<CommonResponse> AdminLogin(string email, string password,int usertype)
