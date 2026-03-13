cbe826264e697f3fb67f9109c0a3c60915963b71
LAPTOP-2P2KMQF4\HP <nasitsmeet28@gmail.com>
2026-03-13 10:32:52 +0100

    feat: add simulation worker service with sensor readings

---
+++ Iot_Management_System_Simulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using Iot_Management_System_Simulation;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// ✅ Register DBContext
+builder.Services.AddSingleton<DBContext>();
+
+// ✅ Register Repository
+builder.Services.AddSingleton<SimulationRepository>();
+
+// ✅ Register Worker
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
